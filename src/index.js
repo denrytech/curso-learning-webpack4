c41abfa2d1b43558ca79af60412af83d02f4fa10
Denry <denrytech@gmail.com>
2019-06-17 09:33:19 -0300

    version 1

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// const $ = require("jquery");
+
+// $("#target").html("Hello world!");
+
+import React from "react"
+import { render } from "react-dom"
+import './style.css'
+
+const Greeting = () => {
+	return (
+		<div>
+			<h1>Hello from React</h1>
+			<div id="image"></div>
+		</div>
+	)
+}
+
+render (
+	<Greeting />,
+	document.getElementById('target')
+)
+
